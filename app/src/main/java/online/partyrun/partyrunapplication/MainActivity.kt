47b9530f071bc117e638c43592ef166b0825873f
nohjunh <nsh042777@naver.com>
2023-06-05 17:03:33 +0900

    feat : init Gradle setting #PAR-55

--- app/src/main/java/online/partyrun/partyrunapplication/MainActivity.kt
+++ app/src/main/java/online/partyrun/partyrunapplication/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import online.partyrun.partyrunapplication.ui.theme.PartyRunApplicationTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

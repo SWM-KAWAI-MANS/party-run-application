46a6196bb52aafce5d32b3d7158c2ec4c66d77ef
nohjunh <nsh042777@naver.com>
2023-06-21 23:10:05 +0900

    refactor : base class들을 core module로 이동 #PAR-88

--- app/src/main/java/online/partyrun/partyrunapplication/core/common/Constants.kt
+++
@@ -1,9 +0,0 @@
-package online.partyrun.partyrunapplication.core.common
-
-import online.partyrun.partyrunapplication.BuildConfig
-
-object Constants {
-    const val FB_GOOGLE_WEB_CLIENT_ID = BuildConfig.FB_GOOGLE_WEB_CLIENT_ID
-    // App 모듈의 Build.gradle.kts -> BuildType에 따른 BASE_URL 분기 처리
-    const val BASE_URL = BuildConfig.BASE_URL
-}
---
+++ core/navigation/src/main/java/online/partyrun/partyrunapplication/core/navigation/main/MainNavRoutes.kt
@@ -0,0 +1,12 @@
+package online.partyrun.partyrunapplication.core.navigation.main
+
+/**
+ * 경로 문자열을 Composable() 메소드에 직접 입력하는 것 대신
+ * sealed 클래스 활용 -> 유연한 경로 정의
+ */
+sealed class MainNavRoutes(val route: String) {
+    object Battle: MainNavRoutes("battle")
+    object Single: MainNavRoutes("single")
+    object Challenge: MainNavRoutes("challenge")
+    object MyPage: MainNavRoutes("myPage")
+}

b6d7b37fe5c7776cbe1dc94ede8bf55ddcb1551a
Junhyeok <njh042777@gmail.com>
2023-07-01 16:17:11 +0900

    feat : Battle 매칭 중 REST API 과정을 위한 Repository 구축 #PAR-117
    [ core/data ]
    - Battle 매칭 과정 중 REST API 통신을 위한 MatchRepository 업데이트

--- core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/MatchRepository.kt
+++ core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/MatchRepository.kt
@@ -1,9 +1,21 @@
 package online.partyrun.partyrunapplication.core.data.repository
 
+import kotlinx.coroutines.flow.Flow
 import okhttp3.sse.EventSource
 import okhttp3.sse.EventSourceListener
+import online.partyrun.partyrunapplication.core.common.network.ApiResponse
+import online.partyrun.partyrunapplication.core.model.match.MatchDecisionRequest
+import online.partyrun.partyrunapplication.core.model.match.MatchStatusResult
+import online.partyrun.partyrunapplication.core.model.match.UserSelectedMatchDistance
 
 interface MatchRepository {
-    fun connectWaitingRunnerEventSource(listener: EventSourceListener): EventSource
+
+    /* REST */
+    suspend fun registerToBattleMatchingQueue(userSelectedMatchDistance: UserSelectedMatchDistance): Flow<ApiResponse<MatchStatusResult>>
+    suspend fun sendAcceptBattleMatchingQueue(matchDecisionRequest: MatchDecisionRequest): Flow<ApiResponse<MatchStatusResult>>
+    suspend fun sendDeclineBattleMatchingQueue(matchDecisionRequest: MatchDecisionRequest): Flow<ApiResponse<MatchStatusResult>>
+
+    /* SSE */
+    fun connectWaitingEventSource(listener: EventSourceListener): EventSource
     fun connectMatchResultEventSource(listener: EventSourceListener): EventSource
 }
--- core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/MatchRepositoryImpl.kt
+++ core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/MatchRepositoryImpl.kt
@@ -1,17 +1,37 @@
 package online.partyrun.partyrunapplication.core.data.repository
 
+import kotlinx.coroutines.flow.Flow
 import okhttp3.sse.EventSource
 import okhttp3.sse.EventSourceListener
 import online.partyrun.partyrunapplication.core.common.Constants.BASE_URL
+import online.partyrun.partyrunapplication.core.common.network.ApiResponse
+import online.partyrun.partyrunapplication.core.common.network.apiRequestFlow
 import online.partyrun.partyrunapplication.core.data.datasource.MatchDataSource
+import online.partyrun.partyrunapplication.core.model.match.MatchDecisionRequest
+import online.partyrun.partyrunapplication.core.model.match.MatchStatusResult
+import online.partyrun.partyrunapplication.core.model.match.UserSelectedMatchDistance
 import javax.inject.Inject
 
 
 class MatchRepositoryImpl @Inject constructor(
     private val dataSource: MatchDataSource
 ) : MatchRepository {
-    override fun connectWaitingRunnerEventSource(listener: EventSourceListener): EventSource {
-        val url = BASE_URL.plus("/api/match/waiting-runner/event")
+    /* REST */
+    override suspend fun registerToBattleMatchingQueue(userSelectedMatchDistance: UserSelectedMatchDistance) = apiRequestFlow {
+        dataSource.registerToBattleMatchingQueue(userSelectedMatchDistance)
+    }
+
+    override suspend fun sendAcceptBattleMatchingQueue(matchDecisionRequest: MatchDecisionRequest) = apiRequestFlow {
+        dataSource.acceptBattleMatchingQueue(matchDecisionRequest)
+    }
+
+    override suspend fun sendDeclineBattleMatchingQueue(matchDecisionRequest: MatchDecisionRequest) = apiRequestFlow {
+        dataSource.declineBattleMatchingQueue(matchDecisionRequest)
+    }
+
+    /* SSE */
+    override fun connectWaitingEventSource(listener: EventSourceListener): EventSource {
+        val url = BASE_URL.plus("/api/waiting/event")
         return dataSource.connectEvent(url = url, listener = listener)
     }
 

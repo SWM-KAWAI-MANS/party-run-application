a1ed355ca6c0362b5a5bd09240f9218e04aae270
Junhyeok <njh042777@gmail.com>
2023-07-01 16:20:41 +0900

    feat : Battle 매칭 중 REST API 과정을 위한 DataSource 구축 #PAR-117
    [ core/data ]
    - Battle 매칭 과정 중 REST API 통신을 위한 MatchDataSource 업데이트

--- core/data/src/main/java/online/partyrun/partyrunapplication/core/data/datasource/MatchDataSource.kt
+++ core/data/src/main/java/online/partyrun/partyrunapplication/core/data/datasource/MatchDataSource.kt
@@ -2,8 +2,15 @@ package online.partyrun.partyrunapplication.core.data.datasource
 
 import okhttp3.sse.EventSource
 import okhttp3.sse.EventSourceListener
+import online.partyrun.partyrunapplication.core.common.network.ApiResult
+import online.partyrun.partyrunapplication.core.model.match.MatchDecisionRequest
+import online.partyrun.partyrunapplication.core.model.match.MatchStatusResult
+import online.partyrun.partyrunapplication.core.model.match.UserSelectedMatchDistance
 
 interface MatchDataSource {
     fun connectEvent(url: String, listener: EventSourceListener): EventSource
 
+    suspend fun registerToBattleMatchingQueue(userSelectedMatchDistance: UserSelectedMatchDistance): ApiResult<MatchStatusResult>
+    suspend fun acceptBattleMatchingQueue(matchDecisionRequest: MatchDecisionRequest): ApiResult<MatchStatusResult>
+    suspend fun declineBattleMatchingQueue(matchDecisionRequest: MatchDecisionRequest): ApiResult<MatchStatusResult>
 }
--- core/data/src/main/java/online/partyrun/partyrunapplication/core/data/datasource/MatchDataSourceImpl.kt
+++ core/data/src/main/java/online/partyrun/partyrunapplication/core/data/datasource/MatchDataSourceImpl.kt
@@ -5,17 +5,35 @@ import okhttp3.Request
 import okhttp3.sse.EventSource
 import okhttp3.sse.EventSourceListener
 import okhttp3.sse.EventSources
+import online.partyrun.partyrunapplication.core.common.network.ApiResult
+import online.partyrun.partyrunapplication.core.model.match.MatchDecisionRequest
+import online.partyrun.partyrunapplication.core.model.match.MatchStatusResult
+import online.partyrun.partyrunapplication.core.model.match.UserSelectedMatchDistance
 import online.partyrun.partyrunapplication.core.network.di.SSEOkHttpClient
 import online.partyrun.partyrunapplication.core.network.di.SSERequestBuilder
+import online.partyrun.partyrunapplication.core.network.service.MatchDecisionApiService
+import online.partyrun.partyrunapplication.core.network.service.WaitingBattleApiService
 import javax.inject.Inject
 
 class MatchDataSourceImpl @Inject constructor(
     @SSEOkHttpClient private val okHttpClient: OkHttpClient,
-    @SSERequestBuilder private val request: Request.Builder
+    @SSERequestBuilder private val request: Request.Builder,
+    private val waitingBattleApi: WaitingBattleApiService,
+    private val matchDecisionApiService: MatchDecisionApiService
 ) : MatchDataSource {
 
     override fun connectEvent(url: String, listener: EventSourceListener): EventSource {
         val request = request.url(url).build()
         return EventSources.createFactory(okHttpClient).newEventSource(request, listener)
     }
+
+    override suspend fun registerToBattleMatchingQueue(userSelectedMatchDistance: UserSelectedMatchDistance): ApiResult<MatchStatusResult> =
+        waitingBattleApi.registerToBattleMatchingQueue(userSelectedMatchDistance)
+
+    override suspend fun acceptBattleMatchingQueue(matchDecisionRequest: MatchDecisionRequest): ApiResult<MatchStatusResult> =
+        matchDecisionApiService.acceptBattleMatchingQueue(matchDecisionRequest)
+
+    override suspend fun declineBattleMatchingQueue(matchDecisionRequest: MatchDecisionRequest): ApiResult<MatchStatusResult> =
+        matchDecisionApiService.declineBattleMatchingQueue(matchDecisionRequest)
+
 }

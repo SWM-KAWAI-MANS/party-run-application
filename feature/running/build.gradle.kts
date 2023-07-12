145f9c4899fea751894fc172234c3ca0a7208771
Junhyeok <njh042777@gmail.com>
2023-07-12 13:24:35 +0900

    feat(module-feature) : running module 생성 #PAR-156
    달리는 과정을 총괄하는 feature

---
+++ feature/running/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("nohjunh.android.feature")
+    id("nohjunh.android.library")
+    id("nohjunh.android.library.compose")
+    id("nohjunh.android.hilt")
+}
+
+android {
+    namespace = "online.partyrun.partyrunapplication.feature.match"
+}
+
+dependencies {
+    // Timber
+    implementation(libs.timber)
+    implementation(libs.gson.code)
+
+    // stomp
+    implementation(libs.stomp.github)
+    // google Location
+    implementation(libs.google.location)
+}

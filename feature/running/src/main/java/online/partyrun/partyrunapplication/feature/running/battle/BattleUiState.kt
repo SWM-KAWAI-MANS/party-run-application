00918e26cc3b4cc6f3774df5a3ac8796ac7f7bf0
Junhyeok <njh042777@gmail.com>
2023-07-14 17:38:10 +0900

    feat : 배틀 프로세스 단계에 따른 유동적인 UI 변경과 onEach 블록 활용 #PAR-163
    1. BattleScreenState의 Flow를 별도로 관리하지 않고 BattleUiState 안에서 처리함으로써 보다 유동적인 UI 변경의 이점을 취함
    
    2. 기존에 추가적인 resultResult.collect { ... } 을 통해 Result 이벤트값들을 처리했다면, onEach{..}을 최대한 활용해 더 간결하게 처리하게끔 수정

--- feature/running/src/main/java/online/partyrun/partyrunapplication/feature/running/battle/BattleUiState.kt
+++ feature/running/src/main/java/online/partyrun/partyrunapplication/feature/running/battle/BattleUiState.kt
@@ -4,6 +4,7 @@ import online.partyrun.partyrunapplication.core.model.battle.BattleState
 
 data class BattleUiState(
     val isConnecting: Boolean = true,
+    val screenState: BattleScreenState = BattleScreenState.Ready,
     val showConnectionError: Boolean = false,
     val battleState: BattleState = BattleState(),
     val timeRemaining: Int = -1,

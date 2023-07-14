244b098c14b3b4023806c6ce02bda880b357ee83
Junhyeok <njh042777@gmail.com>
2023-07-14 20:09:14 +0900

    feat : AuthActivity의 NavGraph Composable화하여 분리 #PAR-160
    1. AuthActivity의 NavHost 부분을 별도 Composable화 분리
    2. 중복되는 Intent 코드 -> 메소드로 추출

--- app/src/main/java/online/partyrun/partyrunapplication/AuthActivity.kt
+++ app/src/main/java/online/partyrun/partyrunapplication/AuthActivity.kt
@@ -5,11 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import online.partyrun.partyrunapplication.core.common.ExtraConstants.EXTRA_FROM_MAIN
@@ -17,9 +13,8 @@ import online.partyrun.partyrunapplication.core.common.ExtraConstants.SPLASH
 import online.partyrun.partyrunapplication.core.common.extension.setIntentActivity
 import online.partyrun.partyrunapplication.core.designsystem.theme.PartyRunApplicationTheme
 import online.partyrun.partyrunapplication.core.navigation.auth.AuthNavRoutes
-import online.partyrun.partyrunapplication.core.navigation.auth.signInRoute
-import online.partyrun.partyrunapplication.core.navigation.auth.splashRoute
 import online.partyrun.partyrunapplication.core.network.GoogleAuthUiClient
+import online.partyrun.partyrunapplication.ui.PartyRunAuth
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -32,50 +27,36 @@ class AuthActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PartyRunApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navController = rememberNavController()
+                val navController = rememberNavController()
+                /* 프로세스 절차 중 유저가 로그아웃 한 경우 혹은 refresh가 만료된 경우 Splash 생략하고 바로 sign_in으로 */
+                val fromMain = intent.getStringExtra(EXTRA_FROM_MAIN)?: SPLASH
 
-                    /* 프로세스 절차 중 유저가 로그아웃 한 경우 혹은 refresh가 만료된 경우 Splash 생략하고 바로 sign_in으로 */
-                    val fromMain = intent.getStringExtra(EXTRA_FROM_MAIN)?: SPLASH
-
-                    NavHost(
-                        navController = navController,
-                        startDestination = fromMain
-                    ) {
-                        splashRoute(
-                            setIntentMainActivity = {
-                                if (googleAuthUiClient.getGoogleAuthUser() != null) {
-                                    setIntentActivity(
-                                        MainActivity::class.java,
-                                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                    )
-                                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-                                    finish()
-                                } else { // 유저가 로그아웃 상태라면 로그인부터 진행
-                                    navController.navigate(AuthNavRoutes.SignIn.route)
-                                }
-                            }
-                        )
-
-                        signInRoute(
-                            googleAuthUiClient = googleAuthUiClient,
-                            setIntentMainActivity = {
-                                /* TODO: Toast 메시지 변경 요 */
-                                Toast.makeText(applicationContext, "Sign in successful", Toast.LENGTH_LONG).show()
-                                setIntentActivity(
-                                    MainActivity::class.java,
-                                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                )
-                                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left) // 화면 전환 애니메이션
-                                finish()
-                            }
-                        )
+                PartyRunAuth(
+                    navController = navController,
+                    startDestination = fromMain,
+                    googleAuthUiClient = googleAuthUiClient,
+                    handleLoginState = {
+                        if (googleAuthUiClient.getGoogleAuthUser() != null) {
+                            intentToMainActivity(toastText = resources.getString(R.string.sign_in_message))
+                        } else { // 유저가 로그아웃 상태라면 로그인부터 진행
+                            navController.navigate(AuthNavRoutes.SignIn.route)
+                        }
+                    },
+                    intentToMainActivity = {
+                        intentToMainActivity(toastText = resources.getString(R.string.sign_in_message))
                     }
-                }
+                )
             }
         }
+    }
+
+    private fun intentToMainActivity(toastText: String) {
+        Toast.makeText(applicationContext, toastText, Toast.LENGTH_LONG).show()
+        setIntentActivity(
+            MainActivity::class.java,
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        )
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left) // 화면 전환 애니메이션
+        finish()
     }
 }

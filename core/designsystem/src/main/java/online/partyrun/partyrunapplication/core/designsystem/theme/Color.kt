670080eb013adf62c000142c110c68018ea21608
Junhyeok <njh042777@gmail.com>
2023-07-16 15:26:16 +0900

    feat : Color Theme 설정 #PAR-165

--- core/designsystem/src/main/java/online/partyrun/partyrunapplication/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/online/partyrun/partyrunapplication/core/designsystem/theme/Color.kt
@@ -2,10 +2,76 @@ package online.partyrun.partyrunapplication.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+/**
+ * Party Run Project colors.
+ */
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+internal val Gray10 = Color(0xFF212427)
+internal val Gray20 = Color(0xFF383D42)
+internal val Gray30 = Color(0xFF50575E)
+internal val Gray40 = Color(0xFF677079)
+internal val Gray50 = Color(0xFF808A93)
+internal val Gray60 = Color(0xFF9A9EAA)
+internal val Gray70 = Color(0xFF9CA3AA)
+internal val Gray80 = Color(0xFFB7BAC2)
+internal val Gray90 = Color(0xFFD3D4DA)
+internal val Gray100 = Color(0xFFEFF0F2)
+
+internal val Purple10 = Color(0xFF5518B4) // Gradient 어두운 쪽
+internal val Purple20 = Color(0xFF601985)
+internal val Purple30 = Color(0xFF8B23CE)
+internal val Purple40 = Color(0xFF9024D0)
+internal val Purple50 = Color(0xFF9927D4)
+internal val Purple60 = Color(0xFFAE52F1) // Primary
+internal val Purple70 = Color(0xFFB42CE1) // Gradient 밝은 쪽
+internal val Purple80 = Color(0xFFB855EC)
+internal val Purple90 = Color(0xFFE493FF)
+
+internal val DimPurple10 = Color(0xFF461D8A)
+internal val DimPurple20 = Color(0xFF892673)
+
+internal val DarkNavy10 = Color(0xFF1C2029) // second background
+internal val DarkNavy20 = Color(0xFF212427)
+internal val DarkNavy30 = Color(0xFF2A2A2A)
+internal val DarkNavy40 = Color(0xFF2D2D2D)
+internal val DarkNavy50 = Color(0xFF282D38)
+internal val DarkNavy60 = Color(0xFF383D42)
+internal val DarkNavy70 = Color(0xFF444444)
+
+internal val White10 = Color(0xFFFFFFFF)
+internal val White20 = Color(0xFFEFF0F2)
+
+internal val DarkGreen10 = Color(0xFF0D1F12)
+internal val DarkGreen20 = Color(0xFF223526)
+internal val DarkGreen30 = Color(0xFF394B3C)
+internal val DarkGreen40 = Color(0xFF4F6352)
+internal val DarkGreen80 = Color(0xFFB7CCB8)
+internal val DarkGreen90 = Color(0xFFD3E8D3)
+internal val DarkGreenGray10 = Color(0xFF1A1C1A)
+internal val DarkGreenGray20 = Color(0xFF2F312E)
+internal val DarkGreenGray90 = Color(0xFFE2E3DE)
+internal val DarkGreenGray95 = Color(0xFFF0F1EC)
+internal val DarkGreenGray99 = Color(0xFFFBFDF7)
+internal val Green10 = Color(0xFF00210B)
+internal val Green20 = Color(0xFF003919)
+internal val Green30 = Color(0xFF005227)
+internal val Green40 = Color(0xFF006D36)
+internal val Green80 = Color(0xFF0EE37C)
+internal val Green90 = Color(0xFF5AFF9D)
+internal val GreenGray30 = Color(0xFF414941)
+internal val GreenGray50 = Color(0xFF727971)
+internal val GreenGray60 = Color(0xFF8B938A)
+internal val GreenGray80 = Color(0xFFC1C9BF)
+internal val GreenGray90 = Color(0xFFDDE5DB)
+internal val Red10 = Color(0xFF410002)
+internal val Red20 = Color(0xFF690005)
+internal val Red30 = Color(0xFF93000A)
+internal val Red40 = Color(0xFFBA1A1A)
+internal val Red80 = Color(0xFFFFB4AB)
+internal val Red90 = Color(0xFFFFDAD6)
+internal val Teal10 = Color(0xFF001F26)
+internal val Teal20 = Color(0xFF02363F)
+internal val Teal30 = Color(0xFF214D56)
+internal val Teal40 = Color(0xFF3A656F)
+internal val Teal80 = Color(0xFFA2CED9)
+internal val Teal90 = Color(0xFFBEEAF6)

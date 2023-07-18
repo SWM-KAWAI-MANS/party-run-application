2d62e95fd11f57138b9bfae3838d98e595d60fcc
Junhyeok <njh042777@gmail.com>
2023-07-18 19:43:46 +0900

    feat : App 모듈-Auth Base Theme 및 Background 셋팅 #PAR-176
    앱의 바탕이 되는 베이스 셋팅 진행

--- app/src/main/java/online/partyrun/partyrunapplication/AuthActivity.kt
+++ app/src/main/java/online/partyrun/partyrunapplication/AuthActivity.kt
@@ -5,12 +5,13 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import online.partyrun.partyrunapplication.core.common.ExtraConstants.EXTRA_FROM_MAIN
 import online.partyrun.partyrunapplication.core.common.ExtraConstants.SPLASH
 import online.partyrun.partyrunapplication.core.common.extension.setIntentActivity
+import online.partyrun.partyrunapplication.core.designsystem.component.PartyRunBackground
 import online.partyrun.partyrunapplication.core.designsystem.theme.PartyRunApplicationTheme
 import online.partyrun.partyrunapplication.core.navigation.auth.AuthNavRoutes
 import online.partyrun.partyrunapplication.core.network.GoogleAuthUiClient
@@ -26,26 +27,32 @@ class AuthActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PartyRunApplicationTheme {
+            PartyRunApplicationTheme(
+                darkTheme = isSystemInDarkTheme(),
+                androidTheme = false,
+                disableDynamicTheming = true,
+            ) {
                 val navController = rememberNavController()
                 /* 프로세스 절차 중 유저가 로그아웃 한 경우 혹은 refresh가 만료된 경우 Splash 생략하고 바로 sign_in으로 */
                 val fromMain = intent.getStringExtra(EXTRA_FROM_MAIN)?: SPLASH
 
-                PartyRunAuth(
-                    navController = navController,
-                    startDestination = fromMain,
-                    googleAuthUiClient = googleAuthUiClient,
-                    handleLoginState = {
-                        if (googleAuthUiClient.getGoogleAuthUser() != null) {
+                PartyRunBackground {
+                    PartyRunAuth(
+                        navController = navController,
+                        startDestination = fromMain,
+                        googleAuthUiClient = googleAuthUiClient,
+                        handleLoginState = {
+                            if (googleAuthUiClient.getGoogleAuthUser() != null) {
+                                intentToMainActivity(toastText = resources.getString(R.string.sign_in_message))
+                            } else { // 유저가 로그아웃 상태라면 로그인부터 진행
+                                navController.navigate(AuthNavRoutes.SignIn.route)
+                            }
+                        },
+                        intentToMainActivity = {
                             intentToMainActivity(toastText = resources.getString(R.string.sign_in_message))
-                        } else { // 유저가 로그아웃 상태라면 로그인부터 진행
-                            navController.navigate(AuthNavRoutes.SignIn.route)
                         }
-                    },
-                    intentToMainActivity = {
-                        intentToMainActivity(toastText = resources.getString(R.string.sign_in_message))
-                    }
-                )
+                    )
+                }
             }
         }
     }

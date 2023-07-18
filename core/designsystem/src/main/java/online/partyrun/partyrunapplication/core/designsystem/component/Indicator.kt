2419545e9d39a8af4d0e82e531f449cac6fd5b3e
Junhyeok <njh042777@gmail.com>
2023-07-18 14:01:51 +0900

    refactor : Indicator, Image Component #PAR-167

--- core/designsystem/src/main/java/online/partyrun/partyrunapplication/core/designsystem/component/Indicator.kt
+++ core/designsystem/src/main/java/online/partyrun/partyrunapplication/core/designsystem/component/Indicator.kt
@@ -1,19 +1,31 @@
 package online.partyrun.partyrunapplication.core.designsystem.component
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
+fun CenterCircularProgressIndicator() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
 fun CenterLottieLoadingIndicator(
     rawAnimation: Int
-){
+) {
     Column(
         modifier = Modifier.fillMaxSize().padding(4.dp),
         verticalArrangement = Arrangement.Center,

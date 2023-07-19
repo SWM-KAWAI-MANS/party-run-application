9705a55f1f7eee8041b4a57688a84b8501a0b510
Junhyeok <njh042777@gmail.com>
2023-07-19 12:39:29 +0900

    feat : SplashScreen 디자인 변경사항 반영 #PAR-177
    1. GradientBackground
    2. 임시 Logo Image

--- feature/splash/src/main/java/online/partyrun/partyrunapplication/feature/splash/SplashScreen.kt
+++ feature/splash/src/main/java/online/partyrun/partyrunapplication/feature/splash/SplashScreen.kt
@@ -1,12 +1,18 @@
 package online.partyrun.partyrunapplication.feature.splash
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import kotlinx.coroutines.delay
-import online.partyrun.partyrunapplication.core.designsystem.component.CenterCircularProgressIndicator
+import online.partyrun.partyrunapplication.core.designsystem.component.PartyRunGradientBackground
+import online.partyrun.partyrunapplication.core.designsystem.theme.LocalGradientColors
 
 @Composable
 fun SplashScreen(
@@ -18,10 +24,19 @@ fun SplashScreen(
         setIntentMainActivity()
     }
 
-    Column(
-        modifier = modifier
-            .fillMaxSize()
+    PartyRunGradientBackground(
+        gradientColors = LocalGradientColors.current
     ) {
-        CenterCircularProgressIndicator()
+        Column(
+            modifier = modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = stringResource(R.string.logo_content_desc)
+            )
+        }
     }
 }

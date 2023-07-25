9ff088a7122b75bcf957909b57918f57279280f3
Junhyeok <njh042777@gmail.com>
2023-07-25 16:55:55 +0900

    test : SignInViewModelTest 리팩토링 #PAR-206

--- feature/sign_in/build.gradle.kts
+++ feature/sign_in/build.gradle.kts
@@ -22,6 +22,6 @@ dependencies {
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.storage)
-
+    testImplementation(project(":core:datastore"))
 
 }

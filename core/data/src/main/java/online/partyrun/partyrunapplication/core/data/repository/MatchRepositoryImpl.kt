cc4fee3a1cfe925d66b18d9b73b0b2419743210c
Junhyeok <njh042777@gmail.com>
2023-07-25 19:06:18 +0900

    refactor : Match와 관련된 Repository에서의 메소드명 간결화
    1. registerToBattleMatchingQueue() -> registerMatch()
    2. acceptBattleMatchingQueue() -> acceptMatch()
    3. declineBattleMatchingQueue() -> declineMatch()

--- core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/MatchRepositoryImpl.kt
+++ core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/MatchRepositoryImpl.kt
@@ -1,14 +1,11 @@
 package online.partyrun.partyrunapplication.core.data.repository
 
-import kotlinx.coroutines.flow.Flow
 import okhttp3.sse.EventSource
 import okhttp3.sse.EventSourceListener
 import online.partyrun.partyrunapplication.core.common.Constants.BASE_URL
-import online.partyrun.partyrunapplication.core.common.network.ApiResponse
 import online.partyrun.partyrunapplication.core.common.network.apiRequestFlow
 import online.partyrun.partyrunapplication.core.data.datasource.MatchDataSource
 import online.partyrun.partyrunapplication.core.model.match.MatchDecisionRequest
-import online.partyrun.partyrunapplication.core.model.match.MatchStatusResult
 import online.partyrun.partyrunapplication.core.model.match.UserSelectedMatchDistance
 import javax.inject.Inject
 
@@ -17,16 +14,16 @@ class MatchRepositoryImpl @Inject constructor(
     private val dataSource: MatchDataSource
 ) : MatchRepository {
     /* REST */
-    override suspend fun registerToBattleMatchingQueue(userSelectedMatchDistance: UserSelectedMatchDistance) = apiRequestFlow {
-        dataSource.registerToBattleMatchingQueue(userSelectedMatchDistance)
+    override suspend fun registerMatch(userSelectedMatchDistance: UserSelectedMatchDistance) = apiRequestFlow {
+        dataSource.registerMatch(userSelectedMatchDistance)
     }
 
-    override suspend fun sendAcceptBattleMatchingQueue(matchDecisionRequest: MatchDecisionRequest) = apiRequestFlow {
-        dataSource.acceptBattleMatchingQueue(matchDecisionRequest)
+    override suspend fun acceptMatch(matchDecisionRequest: MatchDecisionRequest) = apiRequestFlow {
+        dataSource.acceptMatch(matchDecisionRequest)
     }
 
-    override suspend fun sendDeclineBattleMatchingQueue(matchDecisionRequest: MatchDecisionRequest) = apiRequestFlow {
-        dataSource.declineBattleMatchingQueue(matchDecisionRequest)
+    override suspend fun declineMatch(matchDecisionRequest: MatchDecisionRequest) = apiRequestFlow {
+        dataSource.declineMatch(matchDecisionRequest)
     }
 
     /* SSE */

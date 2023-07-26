278f6bdbc12767eddfe4d387afe5177d750c104c
Junhyeok <njh042777@gmail.com>
2023-07-26 16:05:20 +0900

    feat : BattleReadyScreen 디자인 UI 작업 #PAR-198
    1. 디자인 작업
    2. Lottie 추가

--- feature/running/src/main/java/online/partyrun/partyrunapplication/feature/running/battle/ready/BattleReadyScreen.kt
+++ feature/running/src/main/java/online/partyrun/partyrunapplication/feature/running/battle/ready/BattleReadyScreen.kt
@@ -1,19 +1,22 @@
 package online.partyrun.partyrunapplication.feature.running.battle.ready
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import online.partyrun.partyrunapplication.core.designsystem.component.LottieImage
+import online.partyrun.partyrunapplication.core.ui.HeadLine
 import online.partyrun.partyrunapplication.feature.running.R
 
 @Composable
@@ -27,19 +30,12 @@ fun BattleReadyScreen(
 fun ReadyScreenContent(isConnecting: Boolean) {
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White),
+            .fillMaxSize(),
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.White)
-        ) {
-            if (isConnecting) {
-                ConnectingIndicator()
-            } else {
-                ReadyInfo()
-            }
+        if (isConnecting) {
+            ConnectingIndicator()
+        } else {
+            ReadyInfo()
         }
     }
 }
@@ -57,18 +53,31 @@ fun ConnectingIndicator() {
 
 @Composable
 fun ReadyInfo() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = stringResource(id = R.string.comment_before_starts),
-            fontSize = 20.sp
+        LottieImage(
+            modifier = Modifier.padding(top = 200.dp),
+            rawAnimation = R.raw.ready
         )
-        Text(
-            text = stringResource(id = R.string.configuring_tracks),
-            fontSize = 20.sp
-        )
+
+        HeadLine(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 80.dp)
+                .align(Alignment.TopCenter)
+        ) {
+            Text(
+                text = stringResource(id = R.string.comment_before_starts),
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+            Spacer(modifier = Modifier.size(10.dp))
+            Text(
+                text = stringResource(id = R.string.configuring_tracks),
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+        }
     }
 }

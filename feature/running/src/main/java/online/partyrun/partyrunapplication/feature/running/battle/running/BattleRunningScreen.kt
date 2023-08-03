d85d66a35ac9d8607cbc1b25016955639ac12448
Junhyeok <njh042777@gmail.com>
2023-08-03 10:22:21 +0900

    feat : RunningScreen UI 디자인 작업 #PAR-227

--- feature/running/src/main/java/online/partyrun/partyrunapplication/feature/running/battle/running/BattleRunningScreen.kt
+++ feature/running/src/main/java/online/partyrun/partyrunapplication/feature/running/battle/running/BattleRunningScreen.kt
@@ -1,61 +1,340 @@
 package online.partyrun.partyrunapplication.feature.running.battle.running
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import online.partyrun.partyrunapplication.core.model.battle.BattleStatus
+import androidx.hilt.navigation.compose.hiltViewModel
+import online.partyrun.partyrunapplication.core.designsystem.component.PartyRunGradientText
+import online.partyrun.partyrunapplication.core.designsystem.component.PartyRunOutlinedText
+import online.partyrun.partyrunapplication.core.designsystem.component.PartyRunTopAppBar
+import online.partyrun.partyrunapplication.core.designsystem.icon.PartyRunIcons
 import online.partyrun.partyrunapplication.core.model.battle.RunnerStatus
+import online.partyrun.partyrunapplication.core.ui.BackgroundBlurImage
+import online.partyrun.partyrunapplication.core.ui.FormatElapsedTimer
+import online.partyrun.partyrunapplication.feature.running.R
+import online.partyrun.partyrunapplication.feature.running.battle.BattleContentViewModel
+import online.partyrun.partyrunapplication.feature.running.battle.BattleUiState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BattleRunningScreen(
-    battleState: BattleStatus,
+    battleUiState: BattleUiState,
+    battleContentViewModel: BattleContentViewModel = hiltViewModel()
 ) {
-    Spacer(modifier = Modifier.size(30.dp))
-    LazyColumn {
-        items(battleState.battleInfo) { runnerData ->
-            RealtimeBattleScreenItem(runner = runnerData)
-            Spacer(modifier = Modifier.size(10.dp))
+    val battleState = battleUiState.battleState // 배틀 상태 state
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            RunningTopAppBar()
+        }
+    ) { paddingValues ->
+        BackgroundBlurImage(
+            modifier = Modifier.fillMaxSize(),
+            image = R.drawable.background_running_blur,
+            contentAlignment = Alignment.BottomCenter
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    top = paddingValues.calculateTopPadding(),
+                    bottom = paddingValues.calculateBottomPadding()
+                ),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                TrackDistanceDistanceBox(
+                    totalTrackDistance = battleUiState.selectedDistance
+                )
+            }
+            Spacer(modifier = Modifier.size(15.dp))
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    TrackWithMultipleUsers(
+                        battleContentViewModel = battleContentViewModel,
+                        totalTrackDistance = battleUiState.selectedDistance,
+                        runners = battleState.battleInfo
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.size(15.dp))
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Column(
+                        modifier = Modifier.padding(5.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        FormatElapsedTimer(
+                            contentColor = MaterialTheme.colorScheme.onPrimary
+                        )
+                        Row(
+                            modifier = Modifier.padding(top = 10.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = PartyRunIcons.Schedule),
+                                contentDescription = stringResource(id = R.string.ic_schedule),
+                                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
+                            )
+                            Text(
+                                modifier = Modifier.padding(start = 5.dp, bottom = 2.dp),
+                                text = stringResource(id = R.string.progress_time),
+                            )
+                        }
+                    }
+                    Column(
+                        modifier = Modifier.padding(5.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        FormatElapsedTimer(
+                            contentColor = MaterialTheme.colorScheme.onPrimary
+                        )
+                        Row(
+                            modifier = Modifier.padding(top = 10.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = PartyRunIcons.Pace),
+                                contentDescription = stringResource(id = R.string.ic_pace),
+                                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
+                            )
+                            Text(
+                                modifier = Modifier.padding(start = 3.dp, bottom = 2.dp),
+                                text = stringResource(id = R.string.average_pace),
+                            )
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.size(10.dp))
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 25.dp)
+                ) {
+                    // 1등부터 보여주기 위해 오름차순 정렬
+                    val sortedRunners = battleState.battleInfo.sortedBy { it.currentRank }
+                    LazyColumn {
+                        items(sortedRunners) { runnerData ->
+                            RealtimeBattleScreenItem(runner = runnerData)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun RunningTopAppBar() {
+    PartyRunTopAppBar(
+        modifier = Modifier,
+        navigationContent = {
+            IconButton(onClick = { }) {
+                Icon(
+                    painterResource(id = PartyRunIcons.ArrowBackIos),
+                    contentDescription = stringResource(id = R.string.ic_arrow_back)
+                )
+            }
+        },
+        actionsContent = {
+            IconButton(onClick = { }) {
+                Icon(
+                    painterResource(id = PartyRunIcons.Menu),
+                    contentDescription = stringResource(id = R.string.ic_menu)
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun TrackWithMultipleUsers(
+    battleContentViewModel: BattleContentViewModel,
+    totalTrackDistance: Int,
+    runners: List<RunnerStatus>
+) {
+    // 트랙 이미지 리소스를 사용
+    val trackImage = painterResource(R.drawable.runnning_track)
+
+    BoxWithConstraints(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        val context = LocalContext.current
+        val imageBitmap = ImageBitmap.imageResource(context.resources, R.drawable.runnning_track)
+
+        // 트랙 이미지의 실제 비율
+        val aspectRatio = imageBitmap.width.toDouble() / imageBitmap.height.toDouble()
+
+        // 컴포넌트의 최대 크기와 비교하여 가로 또는 세로 크기 조절
+        val trackWidth = this.maxWidth.value / aspectRatio
+        val trackHeight = this.maxHeight.value / aspectRatio
+
+        // 트랙 이미지 표시
+        Image(
+            painter = trackImage,
+            modifier = Modifier.fillMaxSize(),
+            contentDescription = stringResource(id = R.string.track_img)
+        )
+
+        // 각 유저의 위치 표시
+        runners.forEach { runner ->
+            val (currentX, currentY) = battleContentViewModel.mapDistanceToCoordinates(
+                totalTrackDistance = totalTrackDistance.toDouble(),
+                distance = runner.distance,
+                trackWidth = trackWidth,
+                trackHeight = trackHeight
+            )
+            Box(
+                modifier = Modifier.offset(
+                    x = currentX.dp,
+                    y = currentY.dp
+                )
+            ) {
+                Column() {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .height(40.dp)
+                            .background(
+                                color = Color.Black.copy(alpha = 0.5f),
+                                shape = RoundedCornerShape(20.dp) // 모서리를 둥글게 하기 위한 값
+                            )
+                            .padding(10.dp)
+                    ) {
+                        Text(
+                            text = runner.runnerName,
+                            style = MaterialTheme.typography.labelLarge,
+                            color = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                    Image(
+                        modifier = Modifier.size(60.dp),
+                        painter = painterResource(id = R.drawable.runner_img_marker),
+                        contentDescription = stringResource(id = R.string.runner_img_marker)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun RealtimeBattleScreenItem(runner: RunnerStatus) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+            .shadow(4.dp, shape = RoundedCornerShape(35.dp))
+            .border(2.dp, color = MaterialTheme.colorScheme.onBackground, shape = RoundedCornerShape(35.dp)) // 테두리 추가
+            .background(color = MaterialTheme.colorScheme.surface, shape = RoundedCornerShape(35.dp))
+    ) {
+        Row(
+            modifier = Modifier.padding(15.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            if (runner.currentRank == 1) {
+                PartyRunGradientText(
+                    modifier = Modifier.padding(vertical = 8.dp, horizontal = 20.dp)
+                ) {
+                    Text(
+                        text = "${runner.currentRank} " + stringResource(id = R.string.ranking),
+                        style = MaterialTheme.typography.labelLarge,
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+            } else {
+                PartyRunOutlinedText(
+                    modifier = Modifier.padding(vertical = 8.dp, horizontal = 20.dp),
+                    containerColor = Color.Transparent
+                ) {
+                    Text(
+                        text = "${runner.currentRank} " + stringResource(id = R.string.ranking),
+                        style = MaterialTheme.typography.labelLarge,
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.size(15.dp))
+            Text(
+                text = runner.runnerName,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
         }
     }
 }
 
 @Composable
-fun RealtimeBattleScreenItem(runner: RunnerStatus) {
+fun TrackDistanceDistanceBox(
+    modifier: Modifier = Modifier,
+    totalTrackDistance: Int
+) {
     Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-            .shadow(4.dp, shape = RoundedCornerShape(8.dp))
-            .background(Color.White, shape = RoundedCornerShape(8.dp))
-            .padding(15.dp)
+        modifier = modifier.fillMaxSize()
     ) {
-        RunnerStatsColumn(runner)
-    }
-}
-
-@Composable
-fun RunnerStatsColumn(runner: RunnerStatus) {
-    Column {
-        Text(text = "Runner: ${runner.runnerName}")
-        Text(
-            text = "Distance: ${runner.distance} KM",
-            fontSize = 20.sp
+        Image(
+            modifier = modifier
+                .align(Alignment.Center),
+            painter = painterResource(id = R.drawable.distance_display),
+            contentDescription = null
         )
-        Text(text = "Rank: ${runner.currentRank}")
-        Text(text = "Current Round: ${runner.currentRound}")
+        Text(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(start = 30.dp, bottom = 5.dp),
+            text = "$totalTrackDistance" + "m",
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
     }
 }

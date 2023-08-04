8e8dd9f1b10a6e531b097aa55aab4d6a38340a6b
Junhyeok <njh042777@gmail.com>
2023-08-05 08:41:30 +0900

    refactor : TokenExpirationNotifier 네이밍 수정 및 intent bundle 제거
    기존에는 로그아웃 시 스플래시, 약관동의를 생략하고 로그인 창으로 가도록 하기 위한 intent bundle를 제공했지만,
    현재는 로그아웃 시 스플래시, 약관동의 둘 다 보여줄 것이므로 intent bundle 제거

--- app/src/main/java/online/partyrun/partyrunapplication/di/RefreshTokenExpirationNotifier.kt
+++ app/src/main/java/online/partyrun/partyrunapplication/di/RefreshTokenExpirationNotifier.kt
@@ -3,24 +3,22 @@ package online.partyrun.partyrunapplication.di
 import android.content.Context
 import android.content.Intent
 import online.partyrun.partyrunapplication.AuthActivity
-import online.partyrun.partyrunapplication.core.common.ExtraConstants.EXTRA_FROM_MAIN
-import online.partyrun.partyrunapplication.core.common.ExtraConstants.SIGN_IN
 import online.partyrun.partyrunapplication.core.common.extension.setIntentActivity
-import online.partyrun.partyrunapplication.core.common.network.RefreshTokenExpirationNotifier
+import online.partyrun.partyrunapplication.core.common.network.TokenExpirationNotifier
 import timber.log.Timber
 
 class RefreshTokenExpirationNotifier(
     private val context: Context
-): RefreshTokenExpirationNotifier {
+) : TokenExpirationNotifier {
 
     override fun notifyRefreshTokenExpired() {
         Timber.tag("RefreshTokenExpirationNotifier").d("Refresh token expired")
+        /**
+         * 리프래시 토큰 만료 시 Auth부터 다시 진행하도록 수행
+         */
         context.setIntentActivity(
             AuthActivity::class.java,
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        ) {
-            putString(EXTRA_FROM_MAIN, SIGN_IN)
-        }
+        )
     }
-
 }

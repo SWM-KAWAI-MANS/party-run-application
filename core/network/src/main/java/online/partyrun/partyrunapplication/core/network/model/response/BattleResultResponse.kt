855fe22c4f7af7117cc9bcc41a951802fe3ef947
Junhyeok <njh042777@gmail.com>
2023-08-08 15:46:02 +0900

    feat : BattleResultResponse toDomain Mapper 형식화 함수 적용 #PAR-243

--- core/network/src/main/java/online/partyrun/partyrunapplication/core/network/model/response/BattleResultResponse.kt
+++ core/network/src/main/java/online/partyrun/partyrunapplication/core/network/model/response/BattleResultResponse.kt
@@ -2,6 +2,9 @@ package online.partyrun.partyrunapplication.core.network.model.response
 
 import com.google.gson.annotations.SerializedName
 import online.partyrun.partyrunapplication.core.model.running_result.BattleResult
+import online.partyrun.partyrunapplication.core.network.model.util.formatDate
+import online.partyrun.partyrunapplication.core.network.model.util.formatDistanceInKm
+import online.partyrun.partyrunapplication.core.network.model.util.formatDistanceWithComma
 import online.partyrun.partyrunapplication.core.network.model.util.formatTime
 import java.time.LocalDateTime
 
@@ -16,6 +19,9 @@ data class BattleResultResponse(
 
 fun BattleResultResponse.toDomainModel() = BattleResult(
     battleRunnerStatus = this.battleRunnerStatus?.map { it.toDomainModel(this.startTime) } ?: emptyList(),
-    startTime = this.startTime?.let { formatTime(it) } ?: "",
-    targetDistance = this.targetDistance ?: 0
+    startTime = this.startTime?.let { formatTime(it) } ?: "", // "xx:xx" 형식화
+    targetDistance = this.targetDistance ?: 0,
+    targetDistanceFormatted = this.targetDistance?.let { formatDistanceWithComma(it) } ?: "", // 쉼표로 형식화
+    targetDistanceInKm = this.targetDistance?.let { formatDistanceInKm(it) } ?: "",  // km 단위로 형식화
+    battleDate = this.startTime?.let { formatDate(it) } ?: "" // "x월 x일" 형식화
 )

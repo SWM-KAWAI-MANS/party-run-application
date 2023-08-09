bfdbded750662ba8230e04135affb88461da4083
Junhyeok <njh042777@gmail.com>
2023-08-09 12:47:00 +0900

    feat : User 정보 처리를 위한 DataStore Protobuf UseCase 생성 #PAR-263

---
+++ core/domain/src/main/java/online/partyrun/partyrunapplication/core/domain/member/SaveUserDataUseCase.kt
@@ -0,0 +1,15 @@
+package online.partyrun.partyrunapplication.core.domain.member
+
+import online.partyrun.partyrunapplication.core.data.repository.MemberRepository
+import online.partyrun.partyrunapplication.core.model.user.User
+import javax.inject.Inject
+
+class SaveUserDataUseCase @Inject constructor(
+    private val memberRepository: MemberRepository
+) {
+    suspend operator fun invoke(userData: User) {
+        memberRepository.setUserId(userData.id)
+        memberRepository.setUserName(userData.name)
+        memberRepository.setUserProfile(userData.profile)
+    }
+}

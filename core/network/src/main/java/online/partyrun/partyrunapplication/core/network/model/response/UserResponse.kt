e448ad17b345f722d0dc4feff58a43001f65c36a
Junhyeok <njh042777@gmail.com>
2023-08-09 12:42:22 +0900

    feat : core/network/model UserResponse 추가 #PAR-263

---
+++ core/network/src/main/java/online/partyrun/partyrunapplication/core/network/model/response/UserResponse.kt
@@ -0,0 +1,19 @@
+package online.partyrun.partyrunapplication.core.network.model.response
+
+import com.google.gson.annotations.SerializedName
+import online.partyrun.partyrunapplication.core.model.user.User
+
+data class UserResponse(
+    @SerializedName("id")
+    val userId: String?,
+    @SerializedName("name")
+    val userName: String?,
+    @SerializedName("profile")
+    val userProfile: String?,
+)
+
+fun UserResponse.toDomainModel() = User(
+    id = this.userId ?: "",
+    name = this.userName ?: "",
+    profile = this.userProfile ?: ""
+)

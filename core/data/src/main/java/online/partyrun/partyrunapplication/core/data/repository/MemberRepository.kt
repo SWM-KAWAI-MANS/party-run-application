bfea1ae1b769719e8e513518053b48924d7bde0c
Junhyeok <njh042777@gmail.com>
2023-08-09 12:45:35 +0900

    feat : MemberRepository 생성 및 DI 설정 #PAR-263

---
+++ core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/MemberRepository.kt
@@ -0,0 +1,18 @@
+package online.partyrun.partyrunapplication.core.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import online.partyrun.partyrunapplication.core.common.network.ApiResponse
+import online.partyrun.partyrunapplication.core.model.user.User
+
+interface MemberRepository {
+
+    val userData: Flow<User>
+
+    suspend fun getUserData(): Flow<ApiResponse<User>>
+
+    suspend fun setUserName(userName: String)
+
+    suspend fun setUserProfile(userProfile: String)
+
+    suspend fun setUserId(userId: String)
+}

594d322036862a803c7e72a30378052235cef43d
Junhyeok <njh042777@gmail.com>
2023-08-10 17:58:44 +0900

    refactor : RunningResult - > Result 로 네이밍 변경

--- core/domain/src/main/java/online/partyrun/partyrunapplication/core/domain/running_result/GetBattleResultUseCase.kt
+++ core/domain/src/main/java/online/partyrun/partyrunapplication/core/domain/running_result/GetBattleResultUseCase.kt
@@ -2,13 +2,13 @@ package online.partyrun.partyrunapplication.core.domain.running_result
 
 import kotlinx.coroutines.flow.Flow
 import online.partyrun.partyrunapplication.core.common.network.ApiResponse
-import online.partyrun.partyrunapplication.core.data.repository.RunningResultRepository
+import online.partyrun.partyrunapplication.core.data.repository.ResultRepository
 import online.partyrun.partyrunapplication.core.model.running_result.BattleResult
 import javax.inject.Inject
 
 class GetBattleResultUseCase @Inject constructor(
-    private val runningResultRepository: RunningResultRepository
+    private val resultRepository: ResultRepository
 ) {
-    suspend operator fun invoke(battleId: String): Flow<ApiResponse<BattleResult>> =
-        runningResultRepository.getBattleResults(battleId)
+    suspend operator fun invoke(): Flow<ApiResponse<BattleResult>> =
+        resultRepository.getBattleResults()
 }

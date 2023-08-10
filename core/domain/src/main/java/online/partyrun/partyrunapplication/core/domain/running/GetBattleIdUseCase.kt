ff37ed96863d4f3289469a92623f25e0a10f0035
Junhyeok <njh042777@gmail.com>
2023-08-10 17:59:54 +0900

    feat : BattleID 및  Battle 상태를 얻기 위한 UseCase 정의 #PAR-267

---
+++ core/domain/src/main/java/online/partyrun/partyrunapplication/core/domain/running/GetBattleIdUseCase.kt
@@ -0,0 +1,13 @@
+package online.partyrun.partyrunapplication.core.domain.running
+
+import kotlinx.coroutines.flow.Flow
+import online.partyrun.partyrunapplication.core.common.network.ApiResponse
+import online.partyrun.partyrunapplication.core.data.repository.BattleRepository
+import javax.inject.Inject
+
+class GetBattleIdUseCase @Inject constructor(
+    private val battleRepository: BattleRepository
+) {
+    suspend operator fun invoke(): Flow<ApiResponse<String>> =
+        battleRepository.getBattleId()
+}

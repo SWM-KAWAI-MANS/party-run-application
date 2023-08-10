d703fb282fe7367fbd329c7fd9fdd038f7da62af
Junhyeok <njh042777@gmail.com>
2023-08-10 17:21:19 +0900

    refactor : BattleResult 혹은 RunningResult 파일명을 -> Result로 통일

--- core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/ResultRepositoryImpl.kt
+++ core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/ResultRepositoryImpl.kt
@@ -1,21 +1,26 @@
 package online.partyrun.partyrunapplication.core.data.repository
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import online.partyrun.partyrunapplication.core.common.network.ApiResponse
 import online.partyrun.partyrunapplication.core.common.network.apiRequestFlow
+import online.partyrun.partyrunapplication.core.datastore.datasource.BattlePreferencesDataSource
 import online.partyrun.partyrunapplication.core.model.running_result.BattleResult
-import online.partyrun.partyrunapplication.core.network.datasource.RunningResultDataSource
+import online.partyrun.partyrunapplication.core.network.datasource.ResultDataSource
 import online.partyrun.partyrunapplication.core.network.model.response.toDomainModel
 import javax.inject.Inject
 
-class RunningResultRepositoryImpl @Inject constructor(
-    private val runningResultDataSource: RunningResultDataSource
-) : RunningResultRepository {
+class ResultRepositoryImpl @Inject constructor(
+    private val resultDataSource: ResultDataSource,
+    private val battlePreferencesDataSource: BattlePreferencesDataSource
+) : ResultRepository {
 
-    override suspend fun getBattleResults(battleId: String): Flow<ApiResponse<BattleResult>> {
-        return apiRequestFlow { runningResultDataSource.getBattleResults(battleId) }
-            .map { apiResponse ->
+    override suspend fun getBattleResults(): Flow<ApiResponse<BattleResult>> {
+        return apiRequestFlow {
+            val battleId = battlePreferencesDataSource.battleData.first().battleId
+            resultDataSource.getBattleResults(battleId)
+        }.map { apiResponse ->
                 when (apiResponse) {
                     is ApiResponse.Loading -> ApiResponse.Loading
                     is ApiResponse.Success -> ApiResponse.Success(apiResponse.data.toDomainModel())
--- core/network/src/main/java/online/partyrun/partyrunapplication/core/network/datasource/ResultDataSourceImpl.kt
+++ core/network/src/main/java/online/partyrun/partyrunapplication/core/network/datasource/ResultDataSourceImpl.kt
@@ -2,13 +2,13 @@ package online.partyrun.partyrunapplication.core.network.datasource
 
 import online.partyrun.partyrunapplication.core.common.network.ApiResult
 import online.partyrun.partyrunapplication.core.network.model.response.BattleResultResponse
-import online.partyrun.partyrunapplication.core.network.service.BattleResultApiService
+import online.partyrun.partyrunapplication.core.network.service.ResultApiService
 import javax.inject.Inject
 
-class RunningResultDataSourceImpl @Inject constructor(
-    private val battleResultApi: BattleResultApiService
-) : RunningResultDataSource {
+class ResultDataSourceImpl @Inject constructor(
+    private val resultApi: ResultApiService
+) : ResultDataSource {
     override suspend fun getBattleResults(battleId: String): ApiResult<BattleResultResponse> =
-        battleResultApi.getBattleResults(battleId)
+        resultApi.getBattleResults(battleId)
 
 }

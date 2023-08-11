172c610f7f4cae882820c8fe8101e724c2265f9c
Junhyeok <njh042777@gmail.com>
2023-08-11 17:15:01 +0900

    refactor : FinishScreen 전환을 BattleContentScreen에서 수행하도록 변경

--- feature/running/src/main/java/online/partyrun/partyrunapplication/feature/running/battle/BattleScreenState.kt
+++ feature/running/src/main/java/online/partyrun/partyrunapplication/feature/running/battle/BattleScreenState.kt
@@ -3,4 +3,5 @@ package online.partyrun.partyrunapplication.feature.running.battle
 sealed class BattleScreenState {
     object Ready : BattleScreenState()
     object Running : BattleScreenState()
+    object Finish : BattleScreenState()
 }

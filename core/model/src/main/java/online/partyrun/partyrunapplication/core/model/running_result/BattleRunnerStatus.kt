e765bca433e41589f6bf10b444b32b616f852abc
Junhyeok <njh042777@gmail.com>
2023-08-12 18:54:43 +0900

    feat : Running Result data class에 userId 추가 #PAR-281

--- core/model/src/main/java/online/partyrun/partyrunapplication/core/model/running_result/BattleRunnerStatus.kt
+++ core/model/src/main/java/online/partyrun/partyrunapplication/core/model/running_result/BattleRunnerStatus.kt
@@ -2,7 +2,7 @@ package online.partyrun.partyrunapplication.core.model.running_result
 
 data class BattleRunnerStatus(
     val endTime: String = "",
-    val id: String = "", // userId
+    val id: String = "", // 해당 Runner ID
     val name: String = "",
     val profile: String = "",
     val rank: Int = 0,

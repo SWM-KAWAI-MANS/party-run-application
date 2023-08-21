5c8ac2a6c6bc87177741f76072aac43c64eb603a
Junhyeok <njh042777@gmail.com>
2023-08-21 14:41:52 +0900

    refactor : interface DataSource ApiResult -> ApiResponse로 변경 #PAR-301

--- core/network/src/main/java/online/partyrun/partyrunapplication/core/network/datasource/ResultDataSource.kt
+++ core/network/src/main/java/online/partyrun/partyrunapplication/core/network/datasource/ResultDataSource.kt
@@ -1,8 +1,8 @@
 package online.partyrun.partyrunapplication.core.network.datasource
 
-import online.partyrun.partyrunapplication.core.common.network.ApiResult
+import online.partyrun.partyrunapplication.core.common.network.ApiResponse
 import online.partyrun.partyrunapplication.core.network.model.response.BattleResultResponse
 
 interface ResultDataSource {
-    suspend fun getBattleResults(battleId: String): ApiResult<BattleResultResponse>
+    suspend fun getBattleResults(battleId: String): ApiResponse<BattleResultResponse>
 }

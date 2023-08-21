f881249d77a728f5ce3ee9a55425c52b827cc33d
Junhyeok <njh042777@gmail.com>
2023-08-21 23:37:41 +0900

    feat : GoogleAuthRepository signInGoogle() 리턴타입 Custom Result 적용 #PAR-303

--- core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/GoogleAuthRepository.kt
+++ core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/GoogleAuthRepository.kt
@@ -5,12 +5,13 @@ import android.content.IntentSender
 import kotlinx.coroutines.flow.Flow
 import online.partyrun.partyrunapplication.core.model.auth.GoogleUserData
 import online.partyrun.partyrunapplication.core.model.auth.GoogleUserInfo
+import online.partyrun.partyrunapplication.core.common.result.Result
 
 interface GoogleAuthRepository {
     /**
      * Google Sign
      */
-    suspend fun signInGoogle(): IntentSender?
+    suspend fun signInGoogle(): Result<IntentSender?>
     suspend fun signInGoogleWithIntent(intent: Intent): GoogleUserInfo?
     suspend fun signOutGoogleAuth()
     fun getGoogleAuthUser(): Flow<GoogleUserData?>

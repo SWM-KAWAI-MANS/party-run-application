d740c52a1d7e520fd092e3e8a2792b7a5dbac52e
Junhyeok <njh042777@gmail.com>
2023-08-21 14:42:40 +0900

    refactor : Remote DataSource 구현체 ApiResult -> ApiResponse로 변경 #PAR-301

--- core/network/src/main/java/online/partyrun/partyrunapplication/core/network/datasource/SignInDataSourceImpl.kt
+++ core/network/src/main/java/online/partyrun/partyrunapplication/core/network/datasource/SignInDataSourceImpl.kt
@@ -1,7 +1,7 @@
 package online.partyrun.partyrunapplication.core.network.datasource
 
 import online.partyrun.partyrunapplication.core.network.model.response.SignInTokenResponse
-import online.partyrun.partyrunapplication.core.common.network.ApiResult
+import online.partyrun.partyrunapplication.core.common.network.ApiResponse
 import online.partyrun.partyrunapplication.core.network.model.request.GoogleIdTokenRequest
 import online.partyrun.partyrunapplication.core.network.service.SignInApiService
 import javax.inject.Inject
@@ -9,6 +9,8 @@ import javax.inject.Inject
 class SignInDataSourceImpl @Inject constructor(
     private val signApi: SignInApiService
 ) : SignInDataSource {
-    override suspend operator fun invoke(idToken: GoogleIdTokenRequest): ApiResult<SignInTokenResponse> =
+
+    override suspend fun getSignInToken(idToken: GoogleIdTokenRequest): ApiResponse<SignInTokenResponse>  =
         signApi.signInWithGoogleTokenViaServer(idToken)
+
 }

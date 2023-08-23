dbaf184b3e7256be78476ff70d21089e50441036
Junhyeok <njh042777@gmail.com>
2023-08-23 12:47:58 +0900

    feat : BattleRunnerRecordResponse network respose model 추가 #PAR-305
    각 러너들이 뛰어온 정보를 가지고 있는 Response Model 추가

---
+++ core/network/src/main/java/online/partyrun/partyrunapplication/core/network/model/response/BattleRunnerRecordResponse.kt
@@ -0,0 +1,33 @@
+package online.partyrun.partyrunapplication.core.network.model.response
+
+import com.google.gson.annotations.SerializedName
+import online.partyrun.partyrunapplication.core.model.running_result.BattleRunnerRecord
+import online.partyrun.partyrunapplication.core.model.util.DateTimeUtils.localDateTimeFormatter
+import online.partyrun.partyrunapplication.core.network.model.util.formatTime
+import java.time.LocalDateTime
+
+data class BattleRunnerRecordResponse(
+    @SerializedName("altitude")
+    val altitude: Double?,
+    @SerializedName("latitude")
+    val latitude: Double?,
+    @SerializedName("longitude")
+    val longitude: Double?,
+    @SerializedName("time")
+    val time: String?,
+    @SerializedName("distance")
+    val distance: Double?
+)
+
+fun BattleRunnerRecordResponse.toDomainModel(): BattleRunnerRecord {
+
+    val parsedTime =
+        this.time?.let { LocalDateTime.parse(it, localDateTimeFormatter) }
+    return BattleRunnerRecord(
+        altitude = this.altitude ?: 0.0,
+        latitude = this.latitude ?: 0.0,
+        longitude = this.longitude ?: 0.0,
+        time = parsedTime?.let { formatTime(it) } ?: "", // "xx:xx" 형식화
+        distance = this.distance ?: 0.0
+    )
+}

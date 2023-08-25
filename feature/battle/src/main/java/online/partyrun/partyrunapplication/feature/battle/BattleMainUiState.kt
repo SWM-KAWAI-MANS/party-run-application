090a2a34fdeeffdf939183ae808ab77335ff47db
Junhyeok <njh042777@gmail.com>
2023-08-25 15:00:14 +0900

    feat : KmState 추가 #PAR-310

--- feature/battle/src/main/java/online/partyrun/partyrunapplication/feature/battle/BattleMainUiState.kt
+++ feature/battle/src/main/java/online/partyrun/partyrunapplication/feature/battle/BattleMainUiState.kt
@@ -1,5 +1,17 @@
 package online.partyrun.partyrunapplication.feature.battle
 
+enum class KmState {
+    KM_1, KM_3, KM_5, KM_10
+}
+
+fun KmState.toDistance(): Int = when (this) {
+    KmState.KM_1 -> 1000
+    KmState.KM_3 -> 3000
+    KmState.KM_5 -> 5000
+    KmState.KM_10 -> 10000
+}
+
+
 sealed class BattleMainUiState {
     object Loading : BattleMainUiState()
 

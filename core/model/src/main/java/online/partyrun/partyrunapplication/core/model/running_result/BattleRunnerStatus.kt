cf97736adab3d4a1b526eb1875b9bf2102cfc1fa
Junhyeok <njh042777@gmail.com>
2023-08-26 17:32:02 +0900

    feat : 평균 페이스 %02d 포맷으로 리턴 #PAR-315

--- core/model/src/main/java/online/partyrun/partyrunapplication/core/model/running_result/BattleRunnerStatus.kt
+++ core/model/src/main/java/online/partyrun/partyrunapplication/core/model/running_result/BattleRunnerStatus.kt
@@ -43,7 +43,8 @@ private fun formatPace(pace: Double): String {
     val minutesPart = (pace / 60).toInt()
     val secondsPart = (pace % 60).roundToInt()
 
-    return "${minutesPart}'${secondsPart}''"
+    // %02d는 정수를 두 자리로 표현하는데, 만약 한 자리수면 앞에 0 추가
+    return "${minutesPart}'${String.format("%02d", secondsPart)}''"
 }
 
 fun calculateAverageAltitude(runnerStatus: BattleRunnerStatus): Double {

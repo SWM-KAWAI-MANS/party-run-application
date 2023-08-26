51fa227884b9a1c7dc113d1b7fbb3f00a8855224
Junhyeok <njh042777@gmail.com>
2023-08-26 16:52:22 +0900

    feat : 평균 페이스 분/초 계산 로직 변경 #PAR-315

--- core/model/src/main/java/online/partyrun/partyrunapplication/core/model/running_result/BattleRunnerStatus.kt
+++ core/model/src/main/java/online/partyrun/partyrunapplication/core/model/running_result/BattleRunnerStatus.kt
@@ -1,13 +1,14 @@
 package online.partyrun.partyrunapplication.core.model.running_result
 
 import online.partyrun.partyrunapplication.core.model.running_result.ui.BattleRunnerStatusUiModel
+import kotlin.math.roundToInt
 
 data class BattleRunnerStatus(
     val endTime: String = "",
     val id: String = "", // 해당 Runner ID
     val name: String = "",
     val elapsedTime: String = "", // 총 달린 시간
-    val minutesElapsedTime: Long = 0,
+    val secondsElapsedTime: Long = 0,
     val profile: String = "",
     val rank: Int = 0,
     val records: List<BattleRunnerRecord> = listOf() // records 필드 추가
@@ -19,7 +20,7 @@ fun BattleRunnerStatus.toUiModel(): BattleRunnerStatusUiModel {
         id = this.id,
         name = this.name,
         elapsedTime = this.elapsedTime,
-        minutesElapsedTime = this.minutesElapsedTime,
+        secondsElapsedTime = this.secondsElapsedTime,
         profile = this.profile,
         rank = this.rank,
         records = this.records.map { it.toUiModel() },
@@ -28,20 +29,19 @@ fun BattleRunnerStatus.toUiModel(): BattleRunnerStatusUiModel {
     )
 }
 
-// 페이스 = 총 달린 시간(분) / 총 거리(킬로미터)
 fun calculateAveragePace(runnerStatus: BattleRunnerStatus): String {
-    val minutes = runnerStatus.minutesElapsedTime.toDouble()
+    val seconds = runnerStatus.secondsElapsedTime.toDouble()
     val lastRecordDistance = runnerStatus.records.lastOrNull()?.distance ?: 0.0
     val distanceInKm = lastRecordDistance / 1000.0
 
-    val pace = if (distanceInKm > 0) minutes / distanceInKm else 0.0
+    val pace = if (distanceInKm > 0) seconds / distanceInKm else 0.0
 
     return formatPace(pace)
 }
 
 private fun formatPace(pace: Double): String {
-    val minutesPart = pace.toInt()
-    val secondsPart = ((pace - minutesPart) * 60).toInt()
+    val minutesPart = (pace / 60).toInt()
+    val secondsPart = (pace % 60).roundToInt()
 
     return "${minutesPart}'${secondsPart}''"
 }

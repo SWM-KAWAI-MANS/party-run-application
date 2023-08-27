97d072ed97ef056193d221f91f33dda71fe48072
Junhyeok <njh042777@gmail.com>
2023-08-27 21:21:53 +0900

    chore : Remove this unused import #PAR-316

--- feature/running_result/src/main/java/online/partyrun/partyrunapplication/feature/running_result/RunningResultScreen.kt
+++ feature/running_result/src/main/java/online/partyrun/partyrunapplication/feature/running_result/RunningResultScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -44,7 +43,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds

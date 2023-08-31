727e90e5153d7b282472d4d4f48bbad16fc4fca9
Junhyeok <njh042777@gmail.com>
2023-08-31 15:08:24 +0900

    feat : SensorManager Hilt 셋팅 #PAR-321

--- core/data/src/main/java/online/partyrun/partyrunapplication/core/data/di/LocationModule.kt
+++ core/data/src/main/java/online/partyrun/partyrunapplication/core/data/di/LocationModule.kt
@@ -1,6 +1,7 @@
 package online.partyrun.partyrunapplication.core.data.di
 
 import android.content.Context
+import android.hardware.SensorManager
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import dagger.Module
@@ -8,6 +9,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -18,6 +20,12 @@ object LocationModule {
         @ApplicationContext context: Context
     ): FusedLocationProviderClient {
         return LocationServices.getFusedLocationProviderClient(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSensorManager(@ApplicationContext context: Context): SensorManager {
+        return context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
 
 }

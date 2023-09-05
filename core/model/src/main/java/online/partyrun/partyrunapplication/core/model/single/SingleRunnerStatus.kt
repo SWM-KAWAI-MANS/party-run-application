377e63143bed30e0e478bc9bfdf50ef483043459
Junhyeok <njh042777@gmail.com>
2023-09-05 12:55:23 +0900

    feat : Single data class 정의 #PAR-336

---
+++ core/model/src/main/java/online/partyrun/partyrunapplication/core/model/single/SingleRunnerStatus.kt
@@ -0,0 +1,13 @@
+package online.partyrun.partyrunapplication.core.model.single
+
+sealed class ProfileImageSource {
+    data class Url(val url: String) : ProfileImageSource()
+    data class ResourceId(val resId: Int) : ProfileImageSource()
+}
+
+data class SingleRunnerStatus(
+    val runnerName: String = "",
+    val runnerProfile: ProfileImageSource,
+    val distance: Double = 0.0,
+    val currentRank: Int = 0
+)
---
+++ core/model/src/main/java/online/partyrun/partyrunapplication/core/model/single/SingleStatus.kt
@@ -0,0 +1,5 @@
+package online.partyrun.partyrunapplication.core.model.single
+
+data class SingleStatus(
+    val singleInfo: List<SingleRunnerStatus> = emptyList()
+)

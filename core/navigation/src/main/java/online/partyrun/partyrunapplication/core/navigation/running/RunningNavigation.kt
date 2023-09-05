4b97e6f59dea320a60a59135e5ab67ab1349b375
Junhyeok <njh042777@gmail.com>
2023-09-05 10:11:11 +0900

    refactor : BattleRunningNavigation -> RunningNavigation로 네이밍 변경 #PAR-335
    Battle과 Single 모두 한 곳에서 처리하기 위해 RunningNavigation로 통일

--- core/navigation/src/main/java/online/partyrun/partyrunapplication/core/navigation/running/RunningNavigation.kt
+++ core/navigation/src/main/java/online/partyrun/partyrunapplication/core/navigation/running/RunningNavigation.kt
@@ -1,4 +1,4 @@
-package online.partyrun.partyrunapplication.core.navigation.battle_running
+package online.partyrun.partyrunapplication.core.navigation.running
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
@@ -6,13 +6,13 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import online.partyrun.partyrunapplication.core.navigation.main.MainNavRoutes
 import online.partyrun.partyrunapplication.feature.running.battle.BattleContentScreen
+import online.partyrun.partyrunapplication.feature.running.single.SingleContentScreen
 
 fun NavGraphBuilder.battleRunningRoute(
     navigateToBattleOnWebSocketError: () -> Unit,
     navigationToRunningResult: () -> Unit,
     onShowSnackbar: (String) -> Unit
 ) {
-
     composable(
         route = "${MainNavRoutes.BattleRunning.route}?distance={distance}",
         arguments = listOf(navArgument("distance") {

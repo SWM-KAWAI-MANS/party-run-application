ff36790cb805bdef89d858d54dd3a00789e399fe
Junhyeok <njh042777@gmail.com>
2023-09-09 10:27:24 +0900

    refactor : RunningResult로의 네비게이션을 각 모드로 명시적으로 전환하기 위해 BattleResult로 네이밍 변경 #PAR-340

--- core/navigation/src/main/java/online/partyrun/partyrunapplication/core/navigation/running_result/RunningResultNavigation.kt
+++ core/navigation/src/main/java/online/partyrun/partyrunapplication/core/navigation/running_result/RunningResultNavigation.kt
@@ -3,13 +3,13 @@ package online.partyrun.partyrunapplication.core.navigation.running_result
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import online.partyrun.partyrunapplication.core.navigation.main.MainNavRoutes
-import online.partyrun.partyrunapplication.feature.running_result.RunningResultScreen
+import online.partyrun.partyrunapplication.feature.running_result.BattleResultScreen
 
 fun NavGraphBuilder.runningResultRoute(
     navigateToTopLevel: () -> Unit
 ) {
-    composable(route = MainNavRoutes.RunningResult.route) {
-        RunningResultScreen(
+    composable(route = MainNavRoutes.BattleResult.route) {
+        BattleResultScreen(
             navigateToTopLevel = navigateToTopLevel
         )
     }

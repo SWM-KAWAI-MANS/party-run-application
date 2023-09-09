e15fae922dac926d0795774440ab0f5e80e69f59
Junhyeok <njh042777@gmail.com>
2023-09-09 15:36:59 +0900

    refactor : Battle에 있던 계산 메소드를 Single에서도 사용하게끔 FormatLocalDateTime로 이동 #PAR-341

--- core/network/src/main/java/online/partyrun/partyrunapplication/core/network/model/response/BattleRunnerStatusResponse.kt
+++ core/network/src/main/java/online/partyrun/partyrunapplication/core/network/model/response/BattleRunnerStatusResponse.kt
@@ -2,10 +2,10 @@ package online.partyrun.partyrunapplication.core.network.model.response
 
 import com.google.gson.annotations.SerializedName
 import online.partyrun.partyrunapplication.core.model.running_result.battle.BattleRunnerStatus
-import online.partyrun.partyrunapplication.core.model.util.DateTimeUtils.localDateTimeFormatter
-import online.partyrun.partyrunapplication.core.network.model.util.calculateElapsedTime
-import online.partyrun.partyrunapplication.core.network.model.util.calculateSecondsElapsedTime
-import online.partyrun.partyrunapplication.core.network.model.util.formatTime
+import online.partyrun.partyrunapplication.core.network.model.util.calculateElapsedTimeToDomainModel
+import online.partyrun.partyrunapplication.core.network.model.util.calculateSecondsElapsedTimeToDomainModel
+import online.partyrun.partyrunapplication.core.network.model.util.formatEndTime
+import online.partyrun.partyrunapplication.core.network.model.util.parseEndTime
 import java.time.LocalDateTime
 
 data class BattleRunnerStatusResponse(
@@ -33,34 +33,4 @@ fun BattleRunnerStatusResponse.toDomainModel(startTime: LocalDateTime?): BattleR
         secondsElapsedTime = calculateSecondsElapsedTimeToDomainModel(startTime, parsedEndTime),
         records = this.records.map { it.toDomainModel() } // 각 record를 도메인 모델로 변환
     )
-}
-
-private fun calculateElapsedTimeToDomainModel(
-    startTime: LocalDateTime?,
-    parsedEndTime: LocalDateTime?
-) = if (startTime != null && parsedEndTime != null) {
-    calculateElapsedTime(startTime, parsedEndTime)
-} else {
-    "00:00"
-}
-
-private fun calculateSecondsElapsedTimeToDomainModel(
-    startTime: LocalDateTime?,
-    parsedEndTime: LocalDateTime?
-) = if (startTime != null && parsedEndTime != null) {
-    calculateSecondsElapsedTime(startTime, parsedEndTime)
-} else {
-    0
-}
-
-private fun parseEndTime(endTime: String?): LocalDateTime? {
-    /**
-     *  LocalDateTime.parse() :
-     *  문자열 형태로 표현된 날짜와 시간 정보를 LocalDateTime 객체로 변환 수행
-     */
-    return endTime?.let { LocalDateTime.parse(it, localDateTimeFormatter) }
-}
-
-private fun formatEndTime(parsedEndTime: LocalDateTime?): String {
-    return parsedEndTime?.let { formatTime(it) } ?: "00:00"
 }

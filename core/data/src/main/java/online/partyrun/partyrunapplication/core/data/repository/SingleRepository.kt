87a9374a7eb197aff61a06c3462ebc2861139f16
Junhyeok <njh042777@gmail.com>
2023-09-09 15:40:26 +0900

    feat : getGpsDataTest()를 통한 결과 스크린 임시 조회 #PAR-341

--- core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/SingleRepository.kt
+++ core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/SingleRepository.kt
@@ -2,6 +2,7 @@ package online.partyrun.partyrunapplication.core.data.repository
 
 import kotlinx.coroutines.flow.Flow
 import online.partyrun.partyrunapplication.core.model.running.GpsData
+import online.partyrun.partyrunapplication.core.model.running_result.single.SingleResult
 
 interface SingleRepository {
     val gpsDataList: Flow<List<GpsData>>
@@ -10,5 +11,6 @@ interface SingleRepository {
     fun initialize()
     suspend fun addGpsData(gpsData: GpsData)
     suspend fun getGpsData(): List<GpsData>
+    suspend fun getGpsDataTest(): SingleResult
     suspend fun setDistance(totalDistance: Int)
 }

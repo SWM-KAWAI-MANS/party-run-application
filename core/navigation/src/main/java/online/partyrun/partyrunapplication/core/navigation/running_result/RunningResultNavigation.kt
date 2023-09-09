374b614c8864bcac2d655f8401bf4ac2dfceac62
Junhyeok <njh042777@gmail.com>
2023-09-09 10:59:33 +0900

    refactor : BattleResultScreen Import Path 변경 #PAR-340

--- core/navigation/src/main/java/online/partyrun/partyrunapplication/core/navigation/running_result/RunningResultNavigation.kt
+++ core/navigation/src/main/java/online/partyrun/partyrunapplication/core/navigation/running_result/RunningResultNavigation.kt
@@ -3,7 +3,7 @@ package online.partyrun.partyrunapplication.core.navigation.running_result
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import online.partyrun.partyrunapplication.core.navigation.main.MainNavRoutes
-import online.partyrun.partyrunapplication.feature.running_result.BattleResultScreen
+import online.partyrun.partyrunapplication.feature.running_result.battle.BattleResultScreen
 
 fun NavGraphBuilder.runningResultRoute(
     navigateToTopLevel: () -> Unit

9a388f2a6d12f93e05e3e967ea4fb1a753c45425
Junhyeok <njh042777@gmail.com>
2023-09-10 13:07:05 +0900

    feat : Running Service 일시정지, 재시작 Constants 추가 #PAR-343

--- core/common/src/main/java/online/partyrun/partyrunapplication/core/common/Constants.kt
+++ core/common/src/main/java/online/partyrun/partyrunapplication/core/common/Constants.kt
@@ -2,6 +2,7 @@ package online.partyrun.partyrunapplication.core.common
 
 object Constants {
     const val FIREBASE_GOOGLE_CLIENT_ID = BuildConfig.FIREBASE_GOOGLE_CLIENT_ID
+
     // App 모듈의 Build.gradle.kts -> BuildType에 따른 BASE_URL 분기 처리
     const val BASE_URL = BuildConfig.BASE_URL
 
@@ -11,5 +12,7 @@ object Constants {
 
     const val ACTION_START_RUNNING = "StartRunning"
     const val ACTION_STOP_RUNNING = "StopRunning"
+    const val ACTION_PAUSE_RUNNING = "PauseRunning"
+    const val ACTION_RESUME_RUNNING = "ResumeRunning"
     const val BATTLE_ID_KEY = "BATTLE_ID_KEY"
 }

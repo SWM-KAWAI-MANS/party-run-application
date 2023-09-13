588ef6945938ad7ad8ab7698f0b541dd0cab0591
Junhyeok <njh042777@gmail.com>
2023-09-13 13:46:52 +0900

    refactor : SingleViewModel 매직 넘버들을 상수로 정의
    상수 이름들은 대문자와 언더스코어를 사용하여 정의

--- feature/single/src/main/java/online/partyrun/partyrunapplication/feature/single/SingleViewModel.kt
+++ feature/single/src/main/java/online/partyrun/partyrunapplication/feature/single/SingleViewModel.kt
@@ -7,16 +7,24 @@ import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class SingleViewModel @Inject constructor(
+class SingleViewModel @Inject constructor() : ViewModel() {
 
-) : ViewModel() {
+    companion object {
+        const val INITIAL_DISTANCE = 5000 // 'm' 단위
+        const val INITIAL_TIME = 900 // '초' 단위, 900초 = 15분
+        const val DISTANCE_INCREMENT = 250
+        const val TIME_INCREMENT = 300
+        const val SECONDS_IN_HOUR = 3600
+        const val SECONDS_IN_MINUTE = 60
+    }
+
     private val _singleUiState = MutableStateFlow<SingleUiState>(SingleUiState.Success)
     val singleUiState: StateFlow<SingleUiState> = _singleUiState
 
-    private val _targetDistance = MutableStateFlow(5000) // 'm' 단위
+    private val _targetDistance = MutableStateFlow(INITIAL_DISTANCE)
     val targetDistance: StateFlow<Int> = _targetDistance
 
-    private val _targetTime = MutableStateFlow(900) // '초' 단위, 900초 = 15분
+    private val _targetTime = MutableStateFlow(INITIAL_TIME)
     val targetTime: StateFlow<Int> = _targetTime
 
     private val _snackbarMessage = MutableStateFlow("")
@@ -27,29 +35,29 @@ class SingleViewModel @Inject constructor(
     }
 
     fun incrementTargetDistance() {
-        _targetDistance.value += 250
+        _targetDistance.value += DISTANCE_INCREMENT
     }
 
     fun decrementTargetDistance() {
-        if (_targetDistance.value > 250) {
-            _targetDistance.value -= 250
+        if (_targetDistance.value > DISTANCE_INCREMENT) {
+            _targetDistance.value -= DISTANCE_INCREMENT
         }
     }
 
     fun incrementTargetTime() {
-        _targetTime.value += 300
+        _targetTime.value += TIME_INCREMENT
     }
 
     fun decrementTargetTime() {
-        if (_targetTime.value > 300) {
-            _targetTime.value -= 300
+        if (_targetTime.value > TIME_INCREMENT) {
+            _targetTime.value -= TIME_INCREMENT
         }
     }
 
     fun getFormattedTargetTime(): String {
-        val hours = _targetTime.value / 3600
-        val remainingTime = _targetTime.value % 3600
-        val minutes = remainingTime / 60
+        val hours = _targetTime.value / SECONDS_IN_HOUR
+        val remainingTime = _targetTime.value % SECONDS_IN_HOUR
+        val minutes = remainingTime / SECONDS_IN_MINUTE
         return String.format("%02d:%02d", hours, minutes)
     }
 

dab98b6f667601027ea301eff7facf9805a0a802
Junhyeok <njh042777@gmail.com>
2023-09-18 14:19:59 +0900

    chore : 불필요한 Single 컨텐츠 제거 #PAR-353

--- core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/SingleRepository.kt
+++ core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/SingleRepository.kt
@@ -2,7 +2,6 @@ package online.partyrun.partyrunapplication.core.data.repository
 
 import kotlinx.coroutines.flow.Flow
 import online.partyrun.partyrunapplication.core.model.running.RecordDataWithDistance
-import online.partyrun.partyrunapplication.core.model.running_result.single.SingleResult
 import online.partyrun.partyrunapplication.core.model.single.SingleId
 import online.partyrun.partyrunapplication.core.common.result.Result
 import online.partyrun.partyrunapplication.core.model.running.GpsDataWithDistance
@@ -14,10 +13,8 @@ interface SingleRepository {
     fun initialize()
     suspend fun addGpsData(gpsData: GpsDataWithDistance)
     suspend fun getRecordData(): Flow<RecordDataWithDistance>
-    suspend fun getGpsDataTest(): SingleResult
     suspend fun sendRecordData(runningTime: RunningTime): Flow<Result<SingleId>>
 
     suspend fun saveSingleId(singleId: String)
-    suspend fun getSingleId(): Flow<String?>
 
 }
--- core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/SingleRepositoryImpl.kt
+++ core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/SingleRepositoryImpl.kt
@@ -4,18 +4,8 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import online.partyrun.partyrunapplication.core.common.network.apiRequestFlow
 import online.partyrun.partyrunapplication.core.model.running.RecordDataWithDistance
-import online.partyrun.partyrunapplication.core.model.running_result.single.SingleResult
-import online.partyrun.partyrunapplication.core.model.running_result.single.SingleRunnerRecord
-import online.partyrun.partyrunapplication.core.model.running_result.single.SingleRunnerStatus
 import online.partyrun.partyrunapplication.core.model.single.SingleId
 import online.partyrun.partyrunapplication.core.network.datasource.SingleDataSource
-import online.partyrun.partyrunapplication.core.network.model.util.calculateElapsedTimeToDomainModel
-import online.partyrun.partyrunapplication.core.network.model.util.calculateSecondsElapsedTimeToDomainModel
-import online.partyrun.partyrunapplication.core.network.model.util.formatDate
-import online.partyrun.partyrunapplication.core.network.model.util.formatDistanceInKm
-import online.partyrun.partyrunapplication.core.network.model.util.formatDistanceWithComma
-import online.partyrun.partyrunapplication.core.network.model.util.formatEndTime
-import online.partyrun.partyrunapplication.core.network.model.util.formatTime
 import online.partyrun.partyrunapplication.core.common.result.Result
 import online.partyrun.partyrunapplication.core.common.result.mapResultModel
 import online.partyrun.partyrunapplication.core.datastore.datasource.SinglePreferencesDataSource
@@ -47,47 +37,6 @@ class SingleRepositoryImpl @Inject constructor(
 
     override suspend fun saveSingleId(singleId: String) {
         singlePreferencesDataSource.saveSingleId(singleId)
-    }
-
-    override suspend fun getSingleId(): Flow<String?> {
-        return singlePreferencesDataSource.getSingleId()
-    }
-
-    /**
-     * 싱글 UI 테스트를 위한 임시 메소드
-     */
-    override suspend fun getGpsDataTest(): SingleResult {
-        val endTimeLocalDateType = _recordData.value.records.lastOrNull()?.time
-        val startTimeLocalDateType = _recordData.value.records.firstOrNull()?.time
-        val distance = _recordData.value.records.lastOrNull()?.distance?.toInt() ?: 0
-
-        return SingleResult(
-            singleRunnerStatus = SingleRunnerStatus(
-                endTime = formatEndTime(endTimeLocalDateType),
-                elapsedTime = calculateElapsedTimeToDomainModel(
-                    startTimeLocalDateType,
-                    endTimeLocalDateType
-                ),
-                secondsElapsedTime = calculateSecondsElapsedTimeToDomainModel(
-                    startTimeLocalDateType,
-                    endTimeLocalDateType
-                ),
-                records = _recordData.value.records.mapIndexed { _, gpsData ->
-                    SingleRunnerRecord(
-                        altitude = gpsData.altitude,
-                        latitude = gpsData.latitude,
-                        longitude = gpsData.longitude,
-                        time = gpsData.time,
-                        distance = gpsData.distance
-                    )
-                }
-            ),
-            startTime = startTimeLocalDateType?.let { formatTime(it) },
-            targetDistance = distance,
-            targetDistanceFormatted = formatDistanceWithComma(distance),
-            targetDistanceInKm = formatDistanceInKm(distance),
-            singleDate = startTimeLocalDateType?.let { formatDate(it) } ?: ""
-        )
     }
 
     override suspend fun sendRecordData(runningTime: RunningTime): Flow<Result<SingleId>> {

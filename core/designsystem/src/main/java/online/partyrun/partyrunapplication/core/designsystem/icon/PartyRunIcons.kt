3b9725345b4829ff222775280a19b898c3c85ee3
Junhyeok <njh042777@gmail.com>
2023-09-22 12:18:31 +0900

    chore : Copy Icon 추가 #PAR-360

--- core/designsystem/src/main/java/online/partyrun/partyrunapplication/core/designsystem/icon/PartyRunIcons.kt
+++ core/designsystem/src/main/java/online/partyrun/partyrunapplication/core/designsystem/icon/PartyRunIcons.kt
@@ -60,6 +60,7 @@ object PartyRunIcons {
     val edit = R.drawable.ic_round_edit_24
     val PartyJoinIcon = R.drawable.ic_join_rounded_24
     val PartyCreationIcon = R.drawable.ic_creation_rounded_24
+    val CopyIcon = R.drawable.ic_content_copy_24
 }
 
 @Preview(showBackground = true)

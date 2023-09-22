3cb2935ea6bc0577763c3d46fbf5fedde4b2eef3
Junhyeok <njh042777@gmail.com>
2023-09-22 12:18:57 +0900

    feat : PartyCreationTopAppBar 추가 #PAR-360

---
+++ feature/party/src/main/java/online/partyrun/partyrunapplication/feature/party/component/PartyCreationTopAppBar.kt
@@ -0,0 +1,43 @@
+package online.partyrun.partyrunapplication.feature.party.component
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import online.partyrun.partyrunapplication.core.designsystem.component.PartyRunTopAppBar
+import online.partyrun.partyrunapplication.core.designsystem.icon.PartyRunIcons
+import online.partyrun.partyrunapplication.feature.party.R
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PartyCreationTopAppBar(
+    modifier: Modifier,
+    openPartyExitDialog: MutableState<Boolean>
+) {
+    PartyRunTopAppBar(
+        modifier = modifier,
+        navigateToContent = {
+            IconButton(
+                onClick = {
+                    openPartyExitDialog.value = true
+                }
+            ) {
+                Icon(
+                    painterResource(id = PartyRunIcons.ArrowBackIos),
+                    contentDescription = stringResource(id = R.string.ic_arrow_back_desc)
+                )
+            }
+        },
+        titleContent = {
+            Text(
+                text = stringResource(id = R.string.create_party)
+            )
+        }
+    )
+}

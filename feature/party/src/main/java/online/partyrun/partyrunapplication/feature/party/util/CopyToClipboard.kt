b37c8e8a12cb3e0573008811b8282f9c52b42f19
Junhyeok <njh042777@gmail.com>
2023-09-22 12:21:17 +0900

    feat : CopyToClipboard 유틸함수 추가 #PAR-360

---
+++ feature/party/src/main/java/online/partyrun/partyrunapplication/feature/party/util/CopyToClipboard.kt
@@ -0,0 +1,12 @@
+package online.partyrun.partyrunapplication.feature.party.util
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+
+
+fun copyToClipboard(context: Context, text: String) {
+    val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    val clip = ClipData.newPlainText("파티 번호 복사", text)
+    clipboard.setPrimaryClip(clip)
+}

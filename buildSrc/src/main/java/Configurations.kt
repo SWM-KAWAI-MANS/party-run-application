73a74fce06d80b95752a7f127592a9e6757cf8dd
Junhyeok <njh042777@gmail.com>
2023-09-25 20:38:58 +0900

    chore : 버전 코드 변경

--- buildSrc/src/main/java/Configurations.kt
+++ buildSrc/src/main/java/Configurations.kt
@@ -6,7 +6,7 @@ object Configurations {
     private const val MINOR_VERSION = 0
     private const val PATCH_VERSION = 0
     const val VERSION_NAME = "$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
-    const val VERSION_CODE = 1
+    const val VERSION_CODE = 19
     // kotlinCompilerExtensionVersion
     const val COMPOSE = "1.4.5"
 }

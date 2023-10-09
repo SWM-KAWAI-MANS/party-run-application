7d681edeb628539fd30321ecccb7b304df8cf5b5
Junhyeok <njh042777@gmail.com>
2023-10-09 11:55:08 +0900

    feat : getComprehensiveRunRecord Network layer 구축 #PAR-380

--- core/network/src/main/java/online/partyrun/partyrunapplication/core/network/datasource/ResultDataSourceImpl.kt
+++ core/network/src/main/java/online/partyrun/partyrunapplication/core/network/datasource/ResultDataSourceImpl.kt
@@ -2,6 +2,7 @@ package online.partyrun.partyrunapplication.core.network.datasource
 
 import online.partyrun.partyrunapplication.core.common.network.ApiResponse
 import online.partyrun.partyrunapplication.core.network.model.response.BattleResultResponse
+import online.partyrun.partyrunapplication.core.network.model.response.ComprehensiveRunRecordResponse
 import online.partyrun.partyrunapplication.core.network.model.response.SingleResultResponse
 import online.partyrun.partyrunapplication.core.network.service.ResultApiService
 import javax.inject.Inject
@@ -14,5 +15,8 @@ class ResultDataSourceImpl @Inject constructor(
 
     override suspend fun getSingleResults(singleId: String): ApiResponse<SingleResultResponse> =
         resultApi.getSingleResults(singleId)
+
+    override suspend fun getComprehensiveRunRecord(): ApiResponse<ComprehensiveRunRecordResponse> =
+        resultApi.getComprehensiveRunRecord()
 
 }

0a85b801706b487e4086e66000f650ae8a803cbb
Junhyeok <njh042777@gmail.com>
2023-10-10 17:49:38 +0900

    feat : ShimmerEffect Modifier 확장함수 추가 #PAR-380

---
+++ core/designsystem/src/main/java/online/partyrun/partyrunapplication/core/designsystem/component/ShimmerEffect.kt
@@ -0,0 +1,50 @@
+package online.partyrun.partyrunapplication.core.designsystem.component
+
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntSize
+import online.partyrun.partyrunapplication.core.designsystem.theme.Purple50
+import online.partyrun.partyrunapplication.core.designsystem.theme.Purple60
+
+private const val shimmerAnimationDuration = 1000
+
+fun Modifier.shimmerEffect(): Modifier = composed {
+    var size by remember {
+        mutableStateOf(IntSize.Zero)
+    }
+    val transition = rememberInfiniteTransition()
+    val startOffsetX by transition.animateFloat(
+        initialValue = -2 * size.width.toFloat(),
+        targetValue = 2 * size.width.toFloat(),
+        animationSpec = infiniteRepeatable(
+            animation = tween(shimmerAnimationDuration)
+        )
+    )
+
+    background(
+        brush = Brush.linearGradient(
+            colors = listOf(
+                Purple60,
+                Purple50,
+                Purple60,
+            ),
+            start = Offset(startOffsetX, 0f),
+            end = Offset(startOffsetX + size.width.toFloat(), size.height.toFloat())
+        )
+    )
+        .onGloballyPositioned {
+            size = it.size
+        }
+}

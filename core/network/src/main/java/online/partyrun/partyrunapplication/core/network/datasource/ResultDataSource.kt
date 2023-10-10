1717e4cb2d6043e22a2f223c15d6c42c962d8251
Junhyeok <njh042777@gmail.com>
2023-10-10 21:08:58 +0900

    feat : ResultDataSource 싱글 기록 조회 추가 #PAR-381

--- core/network/src/main/java/online/partyrun/partyrunapplication/core/network/datasource/ResultDataSource.kt
+++ core/network/src/main/java/online/partyrun/partyrunapplication/core/network/datasource/ResultDataSource.kt
@@ -3,10 +3,12 @@ package online.partyrun.partyrunapplication.core.network.datasource
 import online.partyrun.partyrunapplication.core.common.network.ApiResponse
 import online.partyrun.partyrunapplication.core.network.model.response.BattleResultResponse
 import online.partyrun.partyrunapplication.core.network.model.response.ComprehensiveRunRecordResponse
+import online.partyrun.partyrunapplication.core.network.model.response.SingleHistoryResponse
 import online.partyrun.partyrunapplication.core.network.model.response.SingleResultResponse
 
 interface ResultDataSource {
     suspend fun getBattleResults(battleId: String): ApiResponse<BattleResultResponse>
     suspend fun getSingleResults(singleId: String): ApiResponse<SingleResultResponse>
     suspend fun getComprehensiveRunRecord(): ApiResponse<ComprehensiveRunRecordResponse>
+    suspend fun getSingleHistory(): ApiResponse<SingleHistoryResponse>
 }

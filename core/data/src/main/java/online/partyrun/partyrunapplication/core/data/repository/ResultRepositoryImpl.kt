a5da463b0cba4eaf3a058f91161afef7c8add9b1
Junhyeok <njh042777@gmail.com>
2023-10-14 13:24:08 +0900

    feat : ResultRepository 싱글, 대결 기록 조회 Room DB에 업데이트 추가 #PAR-383

--- core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/ResultRepositoryImpl.kt
+++ core/data/src/main/java/online/partyrun/partyrunapplication/core/data/repository/ResultRepositoryImpl.kt
@@ -2,22 +2,32 @@ package online.partyrun.partyrunapplication.core.data.repository
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import online.partyrun.partyrunapplication.core.common.network.apiRequestFlow
+import online.partyrun.partyrunapplication.core.common.network.onException
+import online.partyrun.partyrunapplication.core.common.network.onSuccess
 import online.partyrun.partyrunapplication.core.datastore.datasource.BattlePreferencesDataSource
 import online.partyrun.partyrunapplication.core.model.running_result.battle.BattleResult
 import online.partyrun.partyrunapplication.core.network.datasource.ResultDataSource
 import online.partyrun.partyrunapplication.core.network.model.response.toDomainModel
 import online.partyrun.partyrunapplication.core.common.result.Result
 import online.partyrun.partyrunapplication.core.common.result.mapResultModel
+import online.partyrun.partyrunapplication.core.database.dao.BattleRunningHistoryDao
+import online.partyrun.partyrunapplication.core.database.dao.SingleRunningHistoryDao
+import online.partyrun.partyrunapplication.core.database.model.toDomainModel
 import online.partyrun.partyrunapplication.core.datastore.datasource.SinglePreferencesDataSource
 import online.partyrun.partyrunapplication.core.model.my_page.BattleRunningHistory
 import online.partyrun.partyrunapplication.core.model.my_page.ComprehensiveRunRecord
 import online.partyrun.partyrunapplication.core.model.my_page.SingleRunningHistory
 import online.partyrun.partyrunapplication.core.model.running_result.single.SingleResult
+import online.partyrun.partyrunapplication.core.network.model.response.toEntity
 import javax.inject.Inject
 
 class ResultRepositoryImpl @Inject constructor(
     private val resultDataSource: ResultDataSource,
+    private val singleRunningHistoryDao: SingleRunningHistoryDao,
+    private val battleRunningHistoryDao: BattleRunningHistoryDao,
     private val battlePreferencesDataSource: BattlePreferencesDataSource,
     private val singlePreferencesDataSource: SinglePreferencesDataSource
 ) : ResultRepository {
@@ -40,6 +50,28 @@ class ResultRepositoryImpl @Inject constructor(
         return apiRequestFlow {
             resultDataSource.getComprehensiveRunRecord()
         }.mapResultModel { it.toDomainModel() }
+    }
+
+    override suspend fun updateSingleHistory(): Flow<Result<Unit>> = flow {
+        emit(Result.Loading)
+        val response = resultDataSource.getSingleHistory()
+        response.onSuccess { result ->
+            singleRunningHistoryDao.insertAllSingleRunningHistory(result.toEntity())
+            emit(Result.Success(Unit))
+        }.onException { e ->
+            emit(Result.Failure(e.message ?: "UnKnown Error", -1))
+        }
+    }
+
+    override suspend fun updateBattleHistory(): Flow<Result<Unit>> = flow {
+        emit(Result.Loading)
+        val response = resultDataSource.getBattleHistory()
+        response.onSuccess { result ->
+            battleRunningHistoryDao.insertAllBattleRunningHistory(result.toEntity())
+            emit(Result.Success(Unit))
+        }.onException { e ->
+            emit(Result.Failure(e.message ?: "UnKnown Error", -1))
+        }
     }
 
     override suspend fun getSingleHistory(): Flow<Result<SingleRunningHistory>> {

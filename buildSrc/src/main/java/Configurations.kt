ee71b641c0dc460cf9f3f3a7dc01bc329fd48b21
Junhyeok <njh042777@gmail.com>
2023-10-19 15:39:17 +0900

    chore : 버전코드 변경 (v1.3.0)

--- buildSrc/src/main/java/Configurations.kt
+++ buildSrc/src/main/java/Configurations.kt
@@ -3,8 +3,8 @@ object Configurations {
     const val MIN_SDK_VERSION = 26
     const val TARGET_SDK_VERSION = 33
     private const val MAJOR_VERSION = 1
-    private const val MINOR_VERSION = 2
+    private const val MINOR_VERSION = 3
     private const val PATCH_VERSION = 0
     const val VERSION_NAME = "$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
-    const val VERSION_CODE = 22
+    const val VERSION_CODE = 23
 }
